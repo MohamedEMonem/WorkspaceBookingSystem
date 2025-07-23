087cd394ab3e56e3cc71965b3138a6332baace52
Abdelrahman Atef <man1207468@gmail.com>
2025-07-23 21:20:45 +0300

    Add mongoose connection and express JSON middleware

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+//express
 const express = require('express');
 const app = express();
+app.use(express.json())
+//mongoose
+const mongoose = require('mongoose');
 
+
+mongoose.connect("mongodb+srv://mohamedemoniem:UoIKjgHG4DECbuPO@workspacedb.moqih61.mongodb.net/workspaceDB").then(()=>{
+console.log("Connection to DB is Successfully");
+}).catch(()=>{
+console.log("Connection to DB is Failed");
+})
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
 });
