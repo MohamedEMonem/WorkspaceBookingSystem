71756ba75051286e62b7d4781ed0232aec4c2dd2
Abdelrahman Atef <man1207468@gmail.com>
2025-07-30 23:35:43 +0300

    Refactor application routing and structure: restore app-root in index.html, bootstrap App component in main.ts, and add routes for login and registration components in app.routes.ts.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { LoginComponent } from './app/pages/login/login.component';
-import { RegisterComponent } from './app/pages/register/register.component';
 
-bootstrapApplication(RegisterComponent, appConfig)
+bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
