b5f18f3fc2ea16236a65f89c17f22377bbc4e5ab
marwansheriff <marwan.sherrrf2@gmail.com>
2025-07-31 10:12:54 +0300

    home page

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { WorkspaceComponent } from "../../components/workspace/workspace.component";
-import { WorkspacesComponent } from "../workspaces/workspaces.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [WorkspaceComponent, WorkspacesComponent]
+  imports: []
 })
 export class HomeComponent implements OnInit {
 
